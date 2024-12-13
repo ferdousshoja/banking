36a4b9c90cc3152c6832129789fa32660fe6065b
ferdousshoja <fr6@students.kiron.ngo>
2024-12-13 11:47:00 +0100

    Home page UI 53:18 completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 
 const Home =() => {
+    const loggedIn = {firstName: 'Adrian'};
+
     return (
         <section className="home">
             <div className="home-content">
                 <header className="home-header">
-                    Welcome, Adrian
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Acess and manage your account and transactions efficiently"
+                    />
+
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />
                 </header>
             </div>
         </section>
